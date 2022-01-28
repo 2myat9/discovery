e6c6951e3f260090b62a005950058353b9e24a95
Htoomyat Naing <2myat9@gmail.com>
2022-01-27 16:35:31 -0800

    Add youtube link submission feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player/youtube';
 
-function App() {
+const App = () => {
+  const [urlInput, setUrlInput] = useState('')
+  const [video, setVideo] = useState('')
+
+  const handleSubmit = (e: any) => {
+    setVideo(urlInput);
+    e.preventDefault();
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUrlInput(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>Hello World</div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">Paste a url of the video you wish to play: </label>
+        <input type="text" value={urlInput} onChange={handleChange} />
+        <input type="submit" value="Submit" />
+      </form>
+      <ReactPlayer url={video} controls={true} playing={true} volume={0.5} muted={true} />
+    </>
+
+  )
 }
 
 export default App;
