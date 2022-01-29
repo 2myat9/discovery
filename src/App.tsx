a93a116b6c7b9dc315c06b2031dd523df76fb3dc
Htoomyat Naing <2myat9@gmail.com>
2022-01-28 16:11:24 -0800

    Add sass and linters

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,33 @@
 import React, { useState } from 'react';
 import ReactPlayer from 'react-player/youtube';
+import './App.scss';
 
 const App = () => {
-  const [urlInput, setUrlInput] = useState('')
-  const [video, setVideo] = useState('')
+  const [urlInput, setUrlInput] = useState('');
+  const [video, setVideo] = useState('');
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (e: React.FormEvent) => {
     setVideo(urlInput);
     e.preventDefault();
-  }
+  };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUrlInput(e.target.value);
-  }
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setUrlInput(e.currentTarget.value);
+  };
 
   return (
     <>
-      <div>Hello World</div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="">Paste a url of the video you wish to play: </label>
-        <input type="text" value={urlInput} onChange={handleChange} />
-        <input type="submit" value="Submit" />
-      </form>
-      <ReactPlayer url={video} controls={true} playing={true} volume={0.5} muted={true} />
+      <div className="main-container">
+        <div className="welcome-msg">Hello Author</div>
+        <form onSubmit={handleSubmit} className="urlForm">
+          <label htmlFor="urlInput">Paste a url of the video you wish to play: </label>
+          <input type="text" id="urlInput" value={urlInput} onChange={handleChange} />
+          <input type="submit" value="Submit" />
+        </form>
+        <ReactPlayer url={video} controls={true} playing={true} volume={0.5} muted={true} />
+      </div>
     </>
-
-  )
-}
+  );
+};
 
 export default App;
